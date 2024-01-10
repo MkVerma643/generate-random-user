4cd0fa363a6b0628cd1ee03cc5b2d58b70968164
Mukesh Verma <mukeshverma643@gmail.com>
2024-01-10 16:01:04 +0530

    Added the API Code for Generate User

---
+++ src/Card.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Card, CardBody, CardText, CardTitle } from "reactstrap";
+import { FaEnvelope, FaMapMarkedAlt, FaPhone } from "react-icons/fa";
+
+const MyCard = ({ details }) => {
+  return (
+    <Card>
+      <CardBody className="text-center">
+        <img
+          height="200"
+          width="200"
+          className="rounded-circle img-thumbnail border-danger"
+          alt="Random User"
+          src={details.picture?.large}
+        />
+        <CardTitle className="text-primary text-center">
+          <h3>
+            <span>{details.name?.title}</span>&nbsp;
+            <span>{details.name?.first}</span>&nbsp;
+            <span>{details.name?.last}</span>
+          </h3>
+        </CardTitle>
+        <CardText>
+          <FaMapMarkedAlt />
+          {details.location?.city}, {details.location?.country}
+          <p><i>Phone: </i>{details.phone}</p>
+        </CardText>
+        
+      </CardBody>
+    </Card>
+  );
+};
+
+export default MyCard;
