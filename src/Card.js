e3cb57c7244f4efe54f423fe4b291488813e18b8
Mukesh Verma <mukeshverma643@gmail.com>
2024-01-10 16:38:10 +0530

    Added the Heading in project

--- src/Card.js
+++ src/Card.js
@@ -4,30 +4,35 @@ import { FaEnvelope, FaMapMarkedAlt, FaPhone } from "react-icons/fa";
 
 const MyCard = ({ details }) => {
   return (
-    <Card>
-      <CardBody className="text-center">
-        <img
-          height="200"
-          width="200"
-          className="rounded-circle img-thumbnail border-danger"
-          alt="Random User"
-          src={details.picture?.large}
-        />
-        <CardTitle className="text-primary text-center">
-          <h3>
-            <span>{details.name?.title}</span>&nbsp;
-            <span>{details.name?.first}</span>&nbsp;
-            <span>{details.name?.last}</span>
-          </h3>
-        </CardTitle>
-        <CardText>
-          <FaMapMarkedAlt />
-          {details.location?.city}, {details.location?.country}
-          <p><i>Phone: </i>{details.phone}</p>
-        </CardText>
-        
-      </CardBody>
-    </Card>
+    <div>
+      <h2 className="text-center">Generate Random User</h2>
+      <Card>
+        <CardBody className="text-center">
+          <img
+            height="200"
+            width="200"
+            className="rounded-circle img-thumbnail border-danger"
+            alt="Random User"
+            src={details.picture?.large}
+          />
+          <CardTitle className="text-primary text-center">
+            <h3>
+              <span>{details.name?.title}</span>&nbsp;
+              <span>{details.name?.first}</span>&nbsp;
+              <span>{details.name?.last}</span>
+            </h3>
+          </CardTitle>
+          <CardText>
+            <FaMapMarkedAlt />
+            {details.location?.city}, {details.location?.country}
+            <p>
+              <i>Phone: </i>
+              {details.phone}
+            </p>
+          </CardText>
+        </CardBody>
+      </Card>
+    </div>
   );
 };
 
